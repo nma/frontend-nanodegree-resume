b768ca8d713d3fbf54f8390556b072218eefc4e0
nma <nickma38@gmail.com>
2016-09-18 17:30:42 -0400

    setup browser sync for better control of the application

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var cssbeautify = require('gulp-cssbeautify');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('cssbeautify', function() {
+  return gulp.src(['./css/*.css', '!css/bootstrap*.css'])
+    .pipe(cssbeautify())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('build', ['sass', 'cssbeautify']);
+
+gulp.task('serve', ['build'], function() {
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
