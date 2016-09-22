758ce866912a4423a1f11045265c4bbf7196f180
nma <nickma38@gmail.com>
2016-09-22 01:24:57 -0400

    export output by beautifier code

--- js/helper.js
+++ js/helper.js
@@ -228,6 +228,6 @@ window.addEventListener('load', initializeMap);
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
 window.addEventListener('resize', function(e) {
-// Make sure the map bounds get updated on page resize
- map.fitBounds(mapBounds);
+    // Make sure the map bounds get updated on page resize
+    map.fitBounds(mapBounds);
 });
